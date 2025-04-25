77d6c0e7cefab3c0a28051f8353202fce890f655
Michael T. Enders <michael@michael-enders.com>
2025-04-25 14:28:54 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,14 +1,36 @@
 {
   description = "GUI for Ocean Optics spectrometers";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let pkgs = nixpkgs.legacyPackages.${system}; in
-        {
-          devShell = import ./shell.nix { inherit pkgs; };
-        }
-      );
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        pythonEnv = pkgs.python3.withPackages (ps: [
+          ps.numpy
+          ps.seabreeze
+          ps.pyqt5
+          ps.pyqtgraph
+        ]);
+      in
+      {
+        devShell = pkgs.mkShell {
+          QT_QPA_PLATFORM_PLUGIN_PATH = "${pkgs.qt5.qtbase.bin}/lib/qt-${pkgs.qt5.qtbase.version}/plugins";
+          packages = [
+            pythonEnv
+          ];
+        };
+      }
+    );
 }
